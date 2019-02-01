189c6f8c84293370fda224e1226b13352452a851
ingvillajimenez <ing.villa.jimenez@gmail.com>
2019-01-31 18:39:20 -0600

    spring 3

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,13 +1,46 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-class LogIn extends Component {
-    render(){
-        return(
-            <div>
+const formStyles = {
+  width: 400,
+  margin: '50px auto',
+};
 
-            </div>
-        );
+class LogIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      openComponent: false
     }
+  }
+
+  render(){
+    return (
+      <div style={formStyles}>
+        <form onSubmit={ this.login }>
+          <TextField
+            required
+            name="email"
+            label="Email"
+            fullWidth
+            onChange={ this.handleChange }
+          />
+          <TextField
+            required
+            name="password"
+            type="password"
+            label="Password"
+            fullWidth
+            onChange={ this.handleChange }
+          />
+          <Button type='submit' variant='contained'>Login</Button>
+        {/* <Button type='submit' style={ buttonStyle } variant='contained'>Login</Button> */}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LogIn;
